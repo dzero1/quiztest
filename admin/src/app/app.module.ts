342d77ae2bd84d489725604b20e5a44bc9559468
Dananjaya M Perera <dananjaya01@gmail.com>
2017-09-20 12:32:03 +0530

    update

--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MaterialModule }       from '@angular/material';
@@ -7,11 +8,25 @@ import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform
 
 import 'hammerjs';
 
+import { QuizcreatorComponent } from './quizcreator/quizcreator.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'quizcreator',
+    component: QuizcreatorComponent,
+    pathMatch: 'full',
+    data: { title: 'quizcreator' }
+  },
+  //{ path: 'schoolpage/:school', component: SchoolpageComponent },
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuizcreatorComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     MaterialModule,
     BrowserAnimationsModule,
